8fc5860eb3d28dae62c64587180a3b49d8679beb
Luciano Iam <oss@lucianoiam.com>
2022-07-23 09:53:49 +0200

    Use python for the dev mini script
    http.server logs requests which is useful

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-PORT=3000
+PORT=8000
 SCRIPT_DIR=`dirname -- "$0"`
 
 open http://localhost:$PORT?dev=1
-npx serve --symlinks --listen $PORT $SCRIPT_DIR/../src/ui
+python3 -m http.server --directory $SCRIPT_DIR/../src/ui $PORT
 
