cdff0039c4fbdb6a7c0c70220342adc636f8261a
Luciano Iam <oss@lucianoiam.com>
2022-08-01 22:33:07 +0200

    Add missing import
    Actually not needed but good for code clarity.
    dpf.js is not a ES6 module but defines the global DISTRHO symbol.

--- src/ui/lib/modal.js
+++ src/ui/lib/modal.js
@@ -15,7 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
+ 
+import '../dpf.js';
 import { elem, loadHtml, loadStylesheet } from './main.js';
 
 class ModalDialog {
