6ef50c32897907d11290b51e602e0cb4ce086c5f
Luciano Iam <oss@lucianoiam.com>
2022-09-07 11:11:47 +0200

    Enable status for knobs layout

--- src/ConsulPlugin.cpp
+++ src/ConsulPlugin.cpp
@@ -57,7 +57,7 @@ public:
 
     uint32_t getVersion() const override
     {
-        return d_version(1, 3, 0);
+        return d_version(1, 3, 1);
     }
 
     int64_t getUniqueId() const override
--- src/ui/lib/ui.js
+++ src/ui/lib/ui.js
@@ -23,7 +23,7 @@ import { AboutDialog, NetworkDialog, MidiDialog, LayoutDialog } from './dialog.j
 
 function main() {
     DISTRHO.UI.sharedInstance = new ConsulUI({
-        productVersion    : '1.3.0',
+        productVersion    : '1.3.1',
         defaultLayout     : 'mixer',
         controlDescriptor : [
             { name: 'Button', id: 'b', n: 16, cont: false, def: { base: 0   , ch: 1 } },
